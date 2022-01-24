1144b2f298985d7bc31a920d53dc823189e23bca
Carlos Quesada <carlosquesadaumana@gmail.com>
2022-01-24 16:01:17 -0600

    React Router Dom added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import './App.css';
 
-import { Main } from "./components/Main"
-import { Footer } from "./components/Footer"
+import { 
+  HashRouter, 
+  Route, 
+  Routes 
+} from 'react-router-dom';
+
+import { Header } from './components/Header';
+import { HomePage } from './pages/HomePage';
+import { ProjectsPage } from './pages/ProjectsPage';
 
 function App() {
   return (
     <div className="App">
-      <Main />
+      <HashRouter>
+        <Header />
+
+        <Routes>
+          <Route path='/' exact element={<HomePage />} />
+          <Route path='/projects' element={<ProjectsPage />} />
+          <Route path='/projects/:projectId' element={<ProjectsPage />} />
+        </Routes>
+      
+      </HashRouter>
     </div>
   );
 }
