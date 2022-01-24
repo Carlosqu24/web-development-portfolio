3907b9a4cab13016e39cca74e35624b9e3d3bc55
Carlos Quesada <carlosquesadaumana@gmail.com>
2022-01-24 16:01:33 -0600

    Basics pages added

---
+++ src/pages/ProjectsPage/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { Projects } from '../../components/Projects'
+
+export const ProjectsPage = () => {
+      return (
+            <div className="container">
+                  <Projects />
+            </div>
+      )
+}
